182045ee372b7c8dbdd33c41479b2c9752683787
Luke Adams <muhammadadams@gmail.com>
2020-05-12 19:44:22 -0400

    added state

---
+++ team-builder/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import Form from "./Form";
+import Card from "./Card";
+
+function App() {
+  const [members, setMembers] = useState([
+    {
+      id: "1",
+      name: "Luke",
+      email: "muhammadadams@gmail.com",
+      role: "Developer",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <Form />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Form = (props) => {
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input id="email" type="text" name="email" place></input>
+    </form>
+  );
+};
+
+export default Form;
